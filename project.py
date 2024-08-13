4cf5c46911cc7ba84b050af704402eac66c90cda
Haris Ali <malikharis2003@gmail.com>
2024-08-13 21:36:17 +0500

    Initial commit

---
+++ project.py
@@ -0,0 +1,46 @@
+import argparse
+from pathlib import Path
+import re
+import sys
+import mmap
+
+
+def main():
+    # Sets up the skeleton of the program
+    parser = argparse.ArgumentParser(
+        prog="pcat",
+        description="Checks for given pattern in files",
+    )
+
+    # TODO: Need to fix: "TypeError: 'required' is an invalid argument for positionals"
+    parser.add_argument("pattern", type=str, help="Pattern to search for in file(s)")
+
+    # TODO: Allow wildcards
+    parser.add_argument("filename", type=str)
+
+    args = parser.parse_args()
+    file_path = Path(args.filename)
+
+    # encode() converts string (args.pattern) to a bytes object
+    pattern = re.compile(rb"" + args.pattern.encode() + rb"") 
+    
+    if not file_path.exists():
+        sys.exit(f"{args.prog}: {args.filename}: No such file or directory")
+
+    for line in search_pattern(pattern, file_path):
+        print(line)
+
+
+def search_pattern(pattern: re, file_path: Path) -> list:
+    matches = []
+    with open(file_path, "r") as file_obj:
+        with mmap.mmap(file_obj.fileno(), length=0, access=mmap.ACCESS_READ) as mmap_obj:
+            for line in iter(mmap_obj.readline, b""):
+                if re.findall(pattern, line):
+                    # decode() converts byte object to string
+                    matches.append(line.decode())
+
+    return matches
+                
+if __name__ == "__main__":
+    main()
