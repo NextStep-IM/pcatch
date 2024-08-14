c02a29024d8ad4fb7a32231dabe4ddd6a5fac962
Haris Ali <malikharis2003@gmail.com>
2024-08-15 02:17:05 +0500

    Make matched patterns Red

--- project.py
+++ project.py
@@ -4,6 +4,12 @@ import re
 import mmap
 
 
+class Colors:
+    RED = "\033[1;31m"
+    GREEN = "\033[0;32m"
+    RESET = "\033[0;0m"
+
+
 def main():
     # Sets up the skeleton of the program
     parser = argparse.ArgumentParser(
@@ -21,8 +27,8 @@ def main():
     path_list = [Path(arg) for arg in args.FILENAME]
 
     # encode() converts string (args.pattern) to a bytes object
-    pattern = re.compile(rb"" + args.PATTERN.encode(errors="strict") + rb"") 
-    
+    pattern = re.compile(rb"" + args.PATTERN.encode(errors="strict") + rb"")
+
     for line in search_pattern(pattern, path_list):
         print(line)
 
@@ -37,11 +43,17 @@ def search_pattern(pattern: re, file_path: list[Path]) -> list:
             break
         else:
             with file_obj:
-                with mmap.mmap(file_obj.fileno(), length=0, access=mmap.ACCESS_READ) as mmap_obj:
+                with mmap.mmap(
+                    file_obj.fileno(), length=0, access=mmap.ACCESS_READ
+                ) as mmap_obj:
                     for line_num, line in enumerate(iter(mmap_obj.readline, b"")):
-                        if re.findall(pattern, line):
+                        if match := re.search(pattern, line):
                             # decode() converts byte object to string
-                            matches.append(f"{line_num}:{line.decode()}")
+                            colored_str = Colors.RED + match.group().decode() + Colors.RESET
+                            line = re.sub(pattern, colored_str.encode(), line)
+                            matches.append(
+                                f"{Colors.GREEN}{line_num}{Colors.RESET}:{line.decode()}"
+                            )
 
     return matches
 
