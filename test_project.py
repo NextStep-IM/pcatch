98985fd9e078b75379706367546618149f58ef79
Haris Ali <malikharis2003@gmail.com>
2024-08-15 22:43:37 +0500

    Changed function name because of duplicate

--- test_project.py
+++ test_project.py
@@ -1,17 +1,18 @@
 import pytest
 from unittest.mock import patch
-from project import parse_args
+from project import parse_cmd_args
 import sys
-import tempfile # for creating temporary files and dirs
+import tempfile  # for creating temporary files and dirs
+from binaryornot.check import is_binary
+
 
 def test_parse_args():
     test_args = ["pcat", "lorem", "file1.txt", "file2.txt"]
     with patch.object(sys, "argv", test_args):
-        args = parse_args()
-        assert args.PATTERN == ["lorem"] # Type is list because of nargs=1
+        args = parse_cmd_args()
+        assert args.PATTERN == ["lorem"]  # Type is list because of nargs=1
         assert args.FILENAME == ["file1.txt", "file2.txt"]
     test_args = ["pcat"]
     with patch.object(sys, "argv", test_args):
         with pytest.raises(SystemExit):
-            parse_args()
-        
+            parse_cmd_args()
