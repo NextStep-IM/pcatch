2a909c0fffc39ecc9c08a4027ec9a2d026f9842f
Haris Ali <malikharis2003@gmail.com>
2024-08-15 20:31:36 +0500

    Test valid arguments

--- test_project.py
+++ test_project.py
@@ -4,3 +4,9 @@ from project import parse_args
 import sys
 import tempfile # for creating temporary files and dirs
 
+def test_parse_args():
+    test_args = ["pcat", "lorem", "file1.txt", "file2.txt"]
+    with patch.object(sys, "argv", test_args):
+        args = parse_args()
+        assert args.PATTERN == ["lorem"] # Type is list because of nargs=1
+        assert args.FILENAME == ["file1.txt", "file2.txt"]
